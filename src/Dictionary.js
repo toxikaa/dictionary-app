24ba84ab859e924e48e54b9e7820b956cf6aee48
toxikaa <107145902+toxikaa@users.noreply.github.com>
2022-07-20 12:24:27 +0300

    axios added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function submittedForm(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
   }
 
   function changedForm(event) {
     setKeyword(event.target.value);
   }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+  axios.get(apiUrl).then(handleResponse);
 
   return (
     <form className="doctionary-form" onSubmit={submittedForm}>
