df09b84b8de8dbb6b3265d6eefd1ef02f1785a20
toxikaa <107145902+toxikaa@users.noreply.github.com>
2022-07-20 13:15:10 +0300

    added loops and difinitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,27 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
   function submittedForm(event) {
     event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function changedForm(event) {
     setKeyword(event.target.value);
   }
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
-  }
-
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-  axios.get(apiUrl).then(handleResponse);
-
   return (
-    <form className="doctionary-form" onSubmit={submittedForm}>
-      <input type="search" placeholder="Enter a word" onChange={changedForm} />
-    </form>
+    <div className="dictionaryReturn">
+      <form className="doctionary-form" onSubmit={submittedForm}>
+        <input
+          type="search"
+          placeholder="Enter a word"
+          onChange={changedForm}
+        />
+      </form>
+      <Results results={results} />
+    </div>
   );
 }
